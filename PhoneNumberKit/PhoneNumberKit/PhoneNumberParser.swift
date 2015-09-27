6185ebfcadaf10c7b89ff8f60d73d86db5d15492
Roy Marmelstein <marmelroy@gmail.com>
2015-09-27 13:47:03 +0200

    More PhoneNumber parsing progress and initial commit for example app

--- PhoneNumberKit/PhoneNumberKit/PhoneNumberParser.swift
+++ PhoneNumberKit/PhoneNumberKit/PhoneNumberParser.swift
@@ -17,34 +17,90 @@ public class PhoneNumberParser: NSObject {
 
     // MARK: PARSER
 
-    public func parse(rawNumber: String, defaultRegion: String) -> PhoneNumber {
-        let numberToParse = normalizeNonBreakingSpace(rawNumber)
-        let nationalNumber = buildNationalNumber(numberToParse)
+//    public func parse(rawNumber: String, defaultRegion: String) -> PhoneNumber {
+//        let numberToParse = normalizeNonBreakingSpace(rawNumber)
+//        let nationalNumber = buildNationalNumber(numberToParse)
+//        
+//    }
+    
+//    func buildNationalNumber(numberToParse: String) -> String {
+//        var nationalNumber : String = ""
+//        nationalNumber =  nationalNumber.stringByAppendingString("") [(*nationalNumber) stringByAppendingString:[self extractPossibleNumber:numberToParse]];
+//
+//        return nationalNumber
+//    }
+
+    
+    // MARK: PHONE NUMBER HELPERS
+
+    
+    public func extractPossibleNumber(number: NSString) -> String {
+        var possibleNumber : NSString
+        let validStartPattern = "["+PNPlusChars+PNValidDigitsString+"]"
+        let secondNumberStartPattern = "[\\\\\\/] *x";
+        let unwantedEndPattern = "[^" + PNValidDigitsString + "A-Za-z#]+$";
+
+        let start = self.stringPositionByRegex(number, pattern: validStartPattern)
+        if (start >= 0)
+        {
+            possibleNumber = number.substringFromIndex(start)
+            possibleNumber = replaceStringByRegex(possibleNumber, pattern: unwantedEndPattern)
+            let secondNumberStart = self.stringPositionByRegex(number, pattern: secondNumberStartPattern)
+            if (secondNumberStart > 0) {
+                possibleNumber = possibleNumber.substringWithRange(NSMakeRange(0, secondNumberStart - 1))
+            }
+        }
+        else
+        {
+            possibleNumber = "";
+        }
+
+        return possibleNumber as String
     }
     
-    func buildNationalNumber(numberToParse: String) -> String {
-        var nationalNumber : String = ""
-        nationalNumber =  nationalNumber.stringByAppendingString("") [(*nationalNumber) stringByAppendingString:[self extractPossibleNumber:numberToParse]];
+    // MARK: STRING HELPERS
 
-        return nationalNumber
-    }
-
-    
-    // MARK: HELPERS
-
-    func normalizeNonBreakingSpace(string: String) -> String {
+    public func normalizeNonBreakingSpace(string: String) -> String {
         return string.stringByReplacingOccurrencesOfString(PNNonBreakingSpace, withString: " ")
     }
+
+    func stringPositionByRegex(source: NSString, pattern: String) -> Int {
+        do {
+            let regex = try NSRegularExpression(pattern: pattern, options: [])
+            let results = regex.matchesInString(source as String,
+                options: [], range: NSMakeRange(0, source.length))
+            if (results.count > 0) {
+                let match = results.first
+                return (match!.range.location)
+            }
+            return -1
+        } catch {
+            return -1
+        }
+    }
     
-    func extractPossibleNumber(string: String) -> String {
-        let start
-        return string.stringByReplacingOccurrencesOfString(PNNonBreakingSpace, withString: " ")
+    func replaceStringByRegex(source: NSString, pattern: String) -> NSString {
+        var replacementResult : NSString = source
+        do {
+            let regex = try NSRegularExpression(pattern: pattern, options: [])
+            let results = regex.matchesInString(source as String,
+                options: [], range: NSMakeRange(0, source.length))
+            if (results.count == 1) {
+                let range = regex.rangeOfFirstMatchInString(source as String, options: [], range: NSMakeRange(0, source.length))
+                if (range.location != NSNotFound) {
+                    replacementResult = regex.stringByReplacingMatchesInString(source.mutableCopy() as! String, options: [], range: range, withTemplate: "")
+                }
+                return replacementResult
+            }
+            else if (results.count > 1) {
+                replacementResult = regex.stringByReplacingMatchesInString(source.mutableCopy() as! String, options: [], range: NSMakeRange(0, source.length), withTemplate: "")
+            }
+            return replacementResult
+        } catch {
+            return replacementResult
+        }
+
     }
-
-    func stringPositionByRegex(source: String, pattern: String) -> Int {
-
-    }
-
     
 }
 
---
+++ examples/PhoneBook/Sample/Sample/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Roy Marmelstein on 27/09/2015.
+//  Copyright © 2015 Roy Marmelstein. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let testNumberString : NSString = "0689017383\n\nsdsdsds"
+        let numberParser = PhoneNumberParser()
+        let normailzedNumber : NSString = numberParser.normalizeNonBreakingSpace(testNumberString as String)
+        let extractedNumber = numberParser.extractPossibleNumber(normailzedNumber as String)
+        print(testNumberString, normailzedNumber, extractedNumber);
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
