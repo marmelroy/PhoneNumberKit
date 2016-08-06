ccd9a412eb2ad10ac4a939468e0c8673900f0b95
Roy Marmelstein <marmelroy@gmail.com>
2016-08-06 12:47:32 +0200

    Clean up duplicate headers

--- PhoneNumberKit/PhoneNumberKit.h
+++ PhoneNumberKit/PhoneNumberKit.h
@@ -6,7 +6,7 @@
 //  Copyright © 2015 Roy Marmelstein. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import Foundation;
 
 //! Project version number for PhoneNumberKit.
 FOUNDATION_EXPORT double PhoneNumberKitVersionNumber;
