0697e19557941fef08ecd07db43c0ebc911baba6
Michal Srutek <michal.srutek@outlook.com>
2023-07-09 11:19:13 +0200

    Remove unnecessary iOS 11 checks

--- examples/AsYouType/Sample/ViewController.swift
+++ examples/AsYouType/Sample/ViewController.swift
@@ -11,7 +11,7 @@ import Foundation
 import PhoneNumberKit
 import UIKit
 
-class ViewController: UIViewController, CNContactPickerDelegate {
+final class ViewController: UIViewController, CNContactPickerDelegate {
     @IBOutlet var textField: PhoneNumberTextField!
     @IBOutlet var withPrefixSwitch: UISwitch!
     @IBOutlet var withFlagSwitch: UISwitch!
@@ -21,17 +21,13 @@ class ViewController: UIViewController, CNContactPickerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Country picker is only available on >iOS 11.0
-        if #available(iOS 11.0, *) {
-            PhoneNumberKit.CountryCodePicker.commonCountryCodes = ["US", "CA", "MX", "AU", "GB", "DE"]
-        }
+        PhoneNumberKit.CountryCodePicker.commonCountryCodes = ["US", "CA", "MX", "AU", "GB", "DE"]
+
         self.textField.becomeFirstResponder()
         self.withPrefixSwitch.isOn = self.textField.withPrefix
         self.withFlagSwitch.isOn = self.textField.withFlag
         self.withExamplePlaceholderSwitch.isOn = self.textField.withExamplePlaceholder
-        if #available(iOS 11.0, *) {
-            self.withDefaultPickerUISwitch.isOn = self.textField.withDefaultPickerUI
-        }
+        self.withDefaultPickerUISwitch.isOn = self.textField.withDefaultPickerUI
         
         if #available(iOS 13.0, *) {
             self.view.backgroundColor = .systemBackground
@@ -58,8 +54,6 @@ class ViewController: UIViewController, CNContactPickerDelegate {
     }
 
     @IBAction func withDefaultPickerUIDidChange(_ sender: Any) {
-        if #available(iOS 11.0, *) {
-            self.textField.withDefaultPickerUI = self.withDefaultPickerUISwitch.isOn
-        }
+        self.textField.withDefaultPickerUI = self.withDefaultPickerUISwitch.isOn
     }
 }
